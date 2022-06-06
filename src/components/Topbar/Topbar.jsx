9437ba7756b21fe67bd9533ce46c810e49da0d77
mateib47 <m.bucur@student.utwente.nl>
2022-06-06 23:59:20 +0200

    setup, add topbar

---
+++ src/components/Topbar/Topbar.jsx
@@ -0,0 +1,13 @@
+import './topbar.scss'
+
+const Topbar = () => {
+  return (
+    <div className='topbar'>
+        <div className="wrapper">
+            <h1>calculator.</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Topbar
