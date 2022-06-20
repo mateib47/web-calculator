ff43e8642046c0ea88770223d008aef74eb04ae9
mateib47 <m.bucur@student.utwente.nl>
2022-06-20 23:59:15 +0200

    fix buttons alignment and change colors

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -14,24 +14,25 @@ const Calculator = ({expr, setExpr}) => {
   let chars = [
     { char: "AC", id: "clear" },
     { char: "DEL", id: "delete" },
+    { char: ".", id: "decimal" },
+    { char: "/", id: "divide" },
     { char: "1", id: "one" },
     { char: "2", id: "two" },
     { char: "3", id: "three" },
+    { char: "+", id: "add" },
     { char: "4", id: "four" },
     { char: "5", id: "five" },
     { char: "6", id: "six" },
+    { char: "-", id: "subtract" },
     { char: "7", id: "seven" },
     { char: "8", id: "eight" },
     { char: "9", id: "nine" },
-    { char: "0", id: "zero" },
-    { char: "+", id: "add" },
-    { char: "-", id: "subtract" },
     { char: "*", id: "multiply" },
-    { char: "/", id: "divide" },
-    { char: ".", id: "decimal" },
-    { char: "=", id: "equals" },
     { char: "(", id: "lpar" },
     { char: ")", id: "rpar" },
+    { char: "0", id: "zero" },
+    { char: "=", id: "equals" },
+
   ];
   return (
     <div className="calculator">
@@ -41,9 +42,9 @@ const Calculator = ({expr, setExpr}) => {
       </div>
       <div className="bottom">
         <Box sx={{ flexGrow: 1 }}>
-          <Grid container spacing={1}>
+          <Grid container spacing={0.5}>
             {chars.map((c) => (
-              <Grid item xs={3}>
+              <Grid item xs={3} sx={{display:'flex', flexDirection:'column', alignItems:'center', justifyContent:'center' }}>
                 <Button
                   character={c.char}
                   id={c.id}
